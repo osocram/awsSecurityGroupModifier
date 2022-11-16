d700ab5e80aaa3ccbdea4a48287eee88c7b2ef33
Amanda Natallie <amanda.natallie.2@gmail.com>
2022-11-16 04:53:22 -0300

    chore(prettier): run linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from "react";
+import { Flexbox, IntroSection, Home } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Flexbox fullScreen justifyContent="space-evenly">
+      <IntroSection />
+      <Home />
+    </Flexbox>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import theme from "./styles/theme";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import theme, { GlobalStyles } from './theme'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENTID!}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
+)
 
-reportWebVitals();
+reportWebVitals()
